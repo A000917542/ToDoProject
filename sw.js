58173645e2f223287adbbae2e1868220a81d81f5
Ritchie <brent.ritchie@minalytix.net>
2019-11-17 22:32:15 -0500

    optimize sw

--- sw.js
+++ sw.js
@@ -3,41 +3,29 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 workbox.loadModule('workbox-strategies');
 
 workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.css$/,
-    // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-        // Use a custom cache name.
         cacheName: 'css-cache',
     })
 );
 
 workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.js$/,
-    // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-        // Use a custom cache name.
         cacheName: 'js-cache',
     })
 );
 
 workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.htm(l?)$/,
-    // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-        // Use a custom cache name.
         cacheName: 'html-cache',
     })
 );
 
 workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.(?:png|jpg|jpeg|svg|gif)$/,
-    // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-        // Use a custom cache name.
         cacheName: 'img-cache',
     })
 );
