54e2b85b30aa8adcc1418037f8b8686b850f041f
Ritchie <brent.ritchie@minalytix.net>
2019-11-18 00:11:54 -0500

    fix precaches

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-
-
 workbox.routing.registerRoute(
     /\.css$/,
     new workbox.strategies.StaleWhileRevalidate({
